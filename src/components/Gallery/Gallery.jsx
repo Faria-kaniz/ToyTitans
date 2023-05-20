f9b71599f2758c74cc9eff0d46b92fa80b531573
Kaniz Faria <kaniz@batworld.com>
2023-05-20 23:38:52 +0600

    gallery design update

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -3,15 +3,23 @@ import "./Gallery.css";
 
 const Gallery = ({ toysList }) => {
     return (
-        <div className="custom-container">
-            <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        <div className="custom-container mt-4">
+            <h1 className="text-center text-5xl font-bold my-8">Gallery</h1>
+            <hr />
+            <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-4">
                 {toysList.map((ele, index) => {
                     if (index < 12) {
                         return (
-                            <div key={ele._id} className="card w-full h-40">
-                                <figure>
-                                    <img src={ele.picture} alt={ele.name} />
-                                </figure>
+                            <div
+                                key={ele._id}
+                                className="custom-card w-full h-40"
+                            >
+                                <img src={ele.picture} alt={ele.name} />
+                                <div class="hover">
+                                    <div class="line1"></div>
+                                    <div class="line2"></div>
+                                    <div class="sub_tit">{ele.name}</div>
+                                </div>
                             </div>
                         );
                     }
