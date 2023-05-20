b556fda63f79a224622e5d9d7518c13647597592
Kaniz Faria <kaniz@batworld.com>
2023-05-20 21:56:34 +0600

    aos library implemented

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import Gallery from '../../../components/Gallery/Gallery';
-import TabSection from '../../../components/TabSection/TabSection';
-import Banner from '../Banner/Banner';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import React, { useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
+import Gallery from "../../../components/Gallery/Gallery";
+import TabSection from "../../../components/TabSection/TabSection";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
-    const toysList = useLoaderData();  
+    const toysList = useLoaderData();
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            delay: 200,
+        });
+    }, []);
     return (
         <div>
             <Banner />
-            <Gallery toysList={toysList} />
-            <TabSection toysList={toysList} />
+            <div data-aos="fade-up">
+                <Gallery toysList={toysList} />
+            </div>
+            <div data-aos="fade-up">
+                <TabSection toysList={toysList} />
+            </div>
         </div>
     );
 };
